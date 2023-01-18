003025da87be7ef169e7c35b0b8c5f1fc8534886
Caron-Thomas <caron.thomas@gmail.com>
2023-01-18 10:41:14 -0300

    Revert "new table"
    This reverts commit 9e38eb8f8675fb22297fb16a34e7eed5757be4c6.

--- src/main/java/com/br/bookstore/BookstoreApplication.java
+++ src/main/java/com/br/bookstore/BookstoreApplication.java
@@ -1,45 +1,13 @@
 package com.br.bookstore;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import com.br.bookstore.domain.Categoria;
-import com.br.bookstore.domain.Livro;
-import com.br.bookstore.repository.CategoriaRepository;
-import com.br.bookstore.repository.LivroRepository;
 
 @SpringBootApplication
-@EnableJpaRepositories({"repository"})
-@ComponentScan(basePackages = { "domain" })
-@EntityScan({"domain"})   
-public class BookstoreApplication implements CommandLineRunner {
-	
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	@Autowired
-	private LivroRepository livroRepository;
+public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
-		Livro l1 = new Livro(null, "Clean Code", "Robert Martins", "Lorem ipsum", cat1);
-		cat1.getLivros().addAll(Arrays.asList(l1));
-		
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		this.livroRepository.saveAll(Arrays.asList(l1));
-		
-		
 	}
 
 }
