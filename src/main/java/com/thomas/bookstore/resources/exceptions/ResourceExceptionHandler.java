27e89725d6ee9c50ebda75c6025af237f10706af
Caron-Thomas <caron.thomas@gmail.com>
2023-05-22 22:29:49 -0300

    DataIntegrityViolationException

--- src/main/java/com/thomas/bookstore/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/thomas/bookstore/resources/exceptions/ResourceExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.thomas.bookstore.resources.exceptions;
 
-import java.awt.SystemColor;
 
 import javax.servlet.ServletRequest;
 
@@ -9,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.thomas.bookstore.service.exceptions.DataIntegrityViolationException;
 import com.thomas.bookstore.service.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -19,5 +19,12 @@ public class ResourceExceptionHandler {
 		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
 				e.getMessage());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException e, ServletRequest request){
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				e.getMessage());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 }
--- src/main/java/com/thomas/bookstore/service/CategoriaService.java
+++ src/main/java/com/thomas/bookstore/service/CategoriaService.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import com.thomas.bookstore.domain.Categoria;
+import com.thomas.bookstore.dtos.CategoriaDTO;
 import com.thomas.bookstore.repositories.CategoriaRepository;
+import com.thomas.bookstore.service.exceptions.DataIntegrityViolationException;
 import com.thomas.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +33,26 @@ public class CategoriaService {
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDTO) {
+		Categoria obj = findById(id);
+		if(objDTO.getNome() != null)	{	
+				obj.setNome(objDTO.getNome()); }
+		if (objDTO.getDescricao() != null) {
+				obj.setDescricao(objDTO.getDescricao()); }
+		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		findById(id);
+		try {
+			repository.deleteById(id);
+		} catch ( org.springframework.dao.DataIntegrityViolationException e) {
+			throw new com.thomas.bookstore
+							.service.exceptions
+							.DataIntegrityViolationException("Classe não pode ser deletada! Possui livros associados.");
+		}
+		
 	}
 }
