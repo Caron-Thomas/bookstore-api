52baffca38995935e25a55b3d0cdf5e093c964ac
Caron-Thomas <caron.thomas@gmail.com>
2023-05-23 23:27:21 -0300

    crud de livros prontos

--- src/main/java/com/thomas/bookstore/resources/LivroResource.java
+++ src/main/java/com/thomas/bookstore/resources/LivroResource.java
@@ -1,15 +1,22 @@
 package com.thomas.bookstore.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.thomas.bookstore.domain.Livro;
 import com.thomas.bookstore.dtos.LivroDTO;
@@ -37,5 +44,32 @@ public class LivroResource {
 									.collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 		
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj ){
+		Livro newObj = livroService.update(id, obj);
+		return ResponseEntity.ok().body(newObj);
+	}
+	
+	@PatchMapping(value = "/{id}")
+	public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro obj ){
+		Livro newObj = livroService.update(id, obj);
+		return ResponseEntity.ok().body(newObj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Livro> create(@RequestParam(value = "categoria", defaultValue = "0") Integer idCat,
+			@RequestBody Livro obj) {
+		Livro newObj = livroService.create(idCat, obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/livros/{id}")
+				.buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		livroService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/thomas/bookstore/service/LivroService.java
+++ src/main/java/com/thomas/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.thomas.bookstore.domain.Categoria;
 import com.thomas.bookstore.domain.Livro;
 import com.thomas.bookstore.repositories.LivroRepository;
 import com.thomas.bookstore.service.exceptions.ObjectNotFoundException;
@@ -28,6 +29,35 @@ public class LivroService {
 	public List<Livro> findAll(Integer idCat) {
 		categoriaService.findById(idCat);
 		return livroRepository.findAllByCategoria(idCat);
+	}
+
+	public Livro update(Integer id, Livro obj) {
+		Livro newObj = findById(id);
+		updateData(newObj, obj);
+		return livroRepository.save(newObj);
+	}
+
+	private void updateData(Livro newObj, Livro obj) {
+		if(obj.getTitulo() != null) 
+				newObj.setTitulo(obj.getTitulo());
+
+		if(obj.getNomeAutor() != null)
+				newObj.setNomeAutor(obj.getNomeAutor());
+		
+		if(obj.getTexto() != null)
+				newObj.setTexto(obj.getTexto());
+	}
+
+	public Livro create(Integer idCat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(idCat);
+		obj.setCategoria(cat);
+		return livroRepository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		Livro obj = findById(id);
+		livroRepository.deleteById(id);
 	}
 	
 }
